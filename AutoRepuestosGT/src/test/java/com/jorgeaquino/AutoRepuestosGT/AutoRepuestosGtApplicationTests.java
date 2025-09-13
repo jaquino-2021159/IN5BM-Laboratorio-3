dbf445635d09143a914eb992b2790199bbbfe6c3
jaquino-2021159 <jaquino-2021159@kinal.edu.gt>
2025-09-12 22:55:15 -0600

    feat:Se agrega estructura inicial del Segundo Proyecto

---
+++ AutoRepuestosGT/src/test/java/com/jorgeaquino/AutoRepuestosGT/AutoRepuestosGtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgeaquino.AutoRepuestosGT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoRepuestosGtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
